da5eddfc2ce8e2a83142d245c5877c7ac5c7dfa7
lucasdsr <lucasSantana.devl@gmail.com>
2025-03-18 16:48:54 -0300

    feat(eslint-prettier): update eslint-prettier plugin

--- eslint.config.js
+++ eslint.config.js
@@ -4,25 +4,47 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintPluginPrettierRecommended
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
+      'arrow-body-style': ['error', 'as-needed'],
+      'no-unused-vars': [
+        'warn',
+        { vars: 'all', args: 'after-used', ignoreRestSiblings: false }
+      ],
+      'default-param-last': ['warn'],
+      'react/no-unknown-property': ['error', { ignore: ['viewBox'] }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'off',
+      'import/prefer-default-export': 'off',
+      'import/export': 'off',
+      'prettier/prettier': 'error',
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-key': ['off', { checkFragmentShorthand: false }]
+    }
+  }
 )
