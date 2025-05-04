768c3f6ab723e5abbd7b800fbc47946488d0c670
lucasdsr <lucasSantana.devl@gmail.com>
2025-05-03 21:41:42 -0300

    add: email validator

--- src/pages/Register/RegisterForm.tsx
+++ src/pages/Register/RegisterForm.tsx
@@ -1,7 +1,15 @@
-import { Box, Button, Checkbox, Divider } from "@mui/material";
+import { useMemo } from "react";
+
 import { SubmitHandler, useForm } from "react-hook-form";
+
+import { Grid, useTheme } from "@mui/system";
+import { Box, Button, Checkbox, Divider } from "@mui/material";
+
 import * as S from "../../components/Inputs/TextField";
-import { Grid } from "@mui/system";
+
+import GoogleIcon from "@mui/icons-material/Google";
+import AppleIcon from "@mui/icons-material/Apple";
+import { isValidEmail } from "../../utils/validators";
 
 interface IFormFields {
   firstName: string;
@@ -13,10 +21,22 @@ interface IFormFields {
 export const RegisterForm = () => {
   const { register } = useForm();
 
-  const form = useForm<IFormFields>();
+  const theme = useTheme();
+
+  const form = useForm<IFormFields>({ reValidateMode: "onChange" });
+
+  const errors = useMemo(() => form.formState.errors, [form]);
+  console.log("errors", errors);
 
   const onFinish: SubmitHandler<IFormFields> = (values) =>
     console.log("values", values);
+
+  const validateEmail = (value: string) => {
+    console.log("validateEmail");
+    console.log("validateEmail", validateEmail(value));
+    if (!isValidEmail(value)) return "email inválido";
+    return true;
+  };
 
   return (
     <form onSubmit={form.handleSubmit(onFinish)}>
@@ -44,9 +64,14 @@ export const RegisterForm = () => {
               label="email"
               variant="outlined"
               id="outlined-basic"
-              {...register("email")}
+              {...register("email", { validate: validateEmail })}
             />
           </Grid>
+          {errors.email?.message && (
+            <p style={{ color: theme.palette.error }}>
+              {errors.email?.message}
+            </p>
+          )}
           <Grid size={12}>
             <S.TextField
               sx={{ width: "100%" }}
@@ -68,14 +93,14 @@ export const RegisterForm = () => {
         >
           Create account
         </Button>
-        <Divider sx={{ marginBottom: "12px" }}>Or register with</Divider>
+        <Divider sx={{ marginBottom: "24px" }}>Or register with</Divider>
         <Grid container spacing={2}>
           <Grid size={6}>
             <Button
               variant="outlined"
               sx={{ width: "100%", borderColor: "white", color: "white" }}
             >
-              Google
+              <GoogleIcon sx={{ marginRight: "4px" }} />
             </Button>
           </Grid>
           <Grid size={6}>
@@ -83,7 +108,7 @@ export const RegisterForm = () => {
               variant="outlined"
               sx={{ width: "100%", borderColor: "white", color: "white" }}
             >
-              Apple
+              <AppleIcon />
             </Button>
           </Grid>
         </Grid>
